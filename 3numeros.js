579d2eb7a31c1573f76f704443c76e67100d4918
kauefariass <95444539+kauefariass@users.noreply.github.com>
2022-05-22 00:01:47 -0300

    alteração

--- 3numeros.js
+++ 3numeros.js
@@ -1,3 +1,12 @@
 function numeros(){
-    
+let valor1 = parseInt(prompt("Digite um numero"));
+let valor2 = parseInt(prompt("Digite um numero"));
+let valor3 = parseInt(prompt("Digite um numero"));
+if (valor3 < valor1) and (valor1 < valor2);
+alert(valor1 + valor2);
+elif (valor2 < valor1) && (valor1 < valor3);
+alert(valor1 + valor3);
+else {
+alert(valor2 + valor3)
+}
 }
