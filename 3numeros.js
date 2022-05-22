3795b84b3065e3e385ccc18e1702a7d72edd97ca
kauefariass <95444539+kauefariass@users.noreply.github.com>
2022-05-22 01:02:28 -0300

    ok

--- 3numeros.js
+++ 3numeros.js
@@ -3,16 +3,14 @@ function NumerosMaiores(){
 let V1 = parseInt(prompt("Digite um numero"));
 let V2 = parseInt(prompt("Digite um numero"));
 let V3 = parseInt(prompt("Digite um numero"));
-let resultado 
 
-if ( V3 < V1 ) && ( V1 < V2 ){
-    resultado = (valorum + valordois)
+if (V3 < V1 && V1 < V2){
+    alert(V1 + V2);
 }
-else if ( V2 < V1 ) && ( V1 < V3 ){
-    resultado = ( V1 + V3 )
+else if (V2 < V1 && V1 < V3){
+    alert(V1 + V3);
 }
-else {
-    resultado = ( V2 + V3 )
+else{
+    alert(V2 + V3);
 }
-alert(resultado);
 }
