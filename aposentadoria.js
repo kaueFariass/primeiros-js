2d835f23e9b598b3672e3c695aa9c270d9b4d4f5
kauefariass <95444539+kauefariass@users.noreply.github.com>
2022-05-22 18:00:45 -0300

    ok

--- aposentadoria.js
+++ aposentadoria.js
@@ -1,3 +1,24 @@
 function VerificarAposentadoria(){
+
+    let nome = prompt("Digite seu nome");
+    let nascimento = parseInt(prompt("Digite seu ano de nascimento"));
+    let admissao = parseInt(prompt("Digite o ano de admissão na empresa"));
+    let tempoTrab = parseInt(0);
+    let idade = parseInt(0);
+
+    idade = (2022 - nascimento);
+    tempoTrab = (2022 - admissao);
+
     
+    if (idade >= 60 && tempoTrab >= 25) {
+        alert("O funcionário(a) " + nome + " Requerer aposentadoria, pois tem " + idade + " anos de idade e " + tempoTrab + " anos de trabalho.")
+    } else {
+        if (idade < 65) {
+    
+        if (tempoTrab < 30) {
+        alert("O funcionário(a) " + nome + " não Requerer aposentadoria, pois tem apenas " + idade + " anos de idade e " + tempoTrab + " anos de trabalho.");
+    }
+    
+}
+}
 }
