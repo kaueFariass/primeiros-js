f7e48670d7a2e5739c2a3eb716d1df59de7a6b1c
kauefariass <95444539+kauefariass@users.noreply.github.com>
2022-05-25 15:35:12 -0300

    ok

---
+++ media.js
@@ -0,0 +1,28 @@
+function media() {
+    
+    let numero = parseFloat(0);
+    let i = (0);
+    let n = parseInt(0);
+  
+  
+  
+  
+    while (i<=100){
+      let n = parseInt(prompt("Digite um Numero"));
+      
+  
+      if (n % 2 == 0) {
+        numero = numero + n;
+          i = i +1
+      }
+       if (n == 0) {
+        alert("Finalizando");
+        break;
+      }
+  
+    }
+  
+    alert("A média é = " + numero / (i - 1));
+    
+    console.log(i)
+  }
