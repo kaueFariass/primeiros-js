d5548081d2201e96b9c8d9afa4b4b7e2f5d47d77
Kaue <kaue.fsilva1@senacsp.edu.br>
2022-05-25 21:56:04 -0300

    ok

--- temperatura.js
+++ temperatura.js
@@ -1,7 +1,20 @@
 function ConverterTemperatura(){
+
+    if(document.getElementById("Centigrados").value == ""){
+        alert("O preenchimento é obrigatório!");
+        document.getElementById("Centigrados").focus();
+        return;
+    }
  
-    let c = document.getElementById("Centigrados").value;
+    let c = parseFloat(document.getElementById("Centigrados").value);
     let f = (9 * c + 160) / 5;
     document.getElementById("Fahrenheit").value = f;
+ 
+    if (f > 50){
+        document.getElementById("Fahrenheit").style.backgroundColor="red";
+    }
 
+    else {
+        document.getElementById("Fahrenheit").style.backgroundColor="aqua";
+    }
 }
